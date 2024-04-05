ed50c6d35f6c660a0389429d6646d01369f8f812
Bruno <brunomn9@hotmail.com>
2024-04-05 20:39:32 -0300

    tratando os dados

--- grafico.py
+++ grafico.py
@@ -41,47 +41,17 @@ print("asdasd")
 print(r2)
  
 # Create blue bars
-# plt.bar(r1, bars1, width = barWidth, color = 'blue', edgecolor = 'black', capsize=7, label='largura de banda')
+plt.bar(r1, bars1, width = barWidth, color = 'blue', edgecolor = 'black', capsize=7, label='largura de banda')
 
-# # Create cyan bars
-# plt.bar(r2, bars2, width = barWidth, color = 'cyan', edgecolor = 'black', capsize=7, label='taxa de transferência')
+# Create cyan bars
+plt.bar(r2, bars2, width = barWidth, color = 'cyan', edgecolor = 'black', capsize=7, label='taxa de transferência')
  
-# # general layout
-# plt.xticks([r + barWidth for r in range(len(bars1))])
-# plt.ylabel('bps')
-# plt.legend()
-# plt.title(caminho_arquivo)
+# general layout
+plt.xticks([r + barWidth for r in range(len(bars1))])
+plt.ylabel('bps')
+plt.legend()
+plt.title(caminho_arquivo)
  
-# # Show graphic
-# plt.show()
+# Show graphic
+plt.show()
 
-def calcular_media_por_grupo(arquivo):
-    # Dicionário para armazenar a soma e a contagem de cada grupo
-    soma_por_grupo = {}
-    contagem_por_grupo = {}
-
-    # Abrir o arquivo
-    with open(arquivo, 'r') as f:
-        # Ler cada linha do arquivo
-        for linha in f:
-            # Dividir a linha em colunas
-            colunas = linha.strip().split(',')
-
-            # Extrair os valores das colunas 8 e 9
-            valor_coluna8 = float(colunas[7])  # Assumindo que a indexação começa em 0
-            valor_coluna9 = colunas[8]  # Assumindo que a indexação começa em 0
-
-            # Atualizar a soma e a contagem para o grupo correspondente
-            soma_por_grupo[valor_coluna9] = soma_por_grupo.get(valor_coluna9, 0) + valor_coluna8
-            contagem_por_grupo[valor_coluna9] = contagem_por_grupo.get(valor_coluna9, 0) + 1
-
-    # Calcular a média para cada grupo
-    media_por_grupo = {}
-    for grupo, soma in soma_por_grupo.items():
-        contagem = contagem_por_grupo[grupo]
-        media_por_grupo[grupo] = soma / contagem
-
-    return media_por_grupo
-
-print("bbbbbbbb")
-print(calcular_media_por_grupo(caminho_arquivo))
