1aa4b32816cb519f996c78c27c74103aeffe9d34
Daniel <daniel@confirmationcall.com.br>
2024-04-11 23:09:04 -0300

    Revert "add calc avg"
    This reverts commit 6877156c2ce53f4bf6dacef98a009e795b3bcc3e.

--- calc_mean.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('raws.csv', encoding='utf-8-sig', sep='\s*,\s*', engine='python')
-
-res = df.groupby([df.columns[0],df.columns[1]])[[df.columns[2]]].mean().reset_index()
-
-print(res)
